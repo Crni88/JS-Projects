f5cae89225bb0e84eb947751b3627574def3798e
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2020-09-22 13:26:32 +0200

    Added updateTime and updateDate functions

--- Money Converter/script.js
+++ Money Converter/script.js
@@ -6,6 +6,31 @@ const amountElement_two = document.getElementById('amount-two');
 
 const rateElement = document.getElementById('rate');
 const swap = document.getElementById('swap');
+
+//Updates the date
+function updateDate(d) {
+    const dateHtml = document.getElementById("date");
+    dateHtml.innerHTML += d;
+}
+
+function updateTime(t) {
+    console.log(typeof (t));
+    var digits = t.toString().split('');
+    var realDigits = digits.map(Number);
+    //console.log("Stack overflow", realDigits);
+    //Gets hours
+    var hours = realDigits.splice(0, 2);
+    hours = hours.toString().replace(',', '');
+    //Gets minutes 
+    var minutes = realDigits.splice(0, 2);
+    minutes = minutes.toString().replace(',', '');
+    //Gets seconds 
+    var seconds = realDigits.splice(0, 2);
+    seconds = seconds.toString().replace(',', '');
+    //Updates the time 
+    const timeHtml = document.getElementById("time");
+    timeHtml.innerHTML += hours + ":" + minutes + ":" + seconds;
+}
 //Fetch exchange rate and update the DOM
 function calculate() {
 
@@ -16,7 +41,9 @@ function calculate() {
     fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
         .then(res => res.json()) //Promise from the server
         .then(data => { //List of atributes 
-            // console.log(data);
+            //console.log(data);
+            updateDate(data.date);
+            updateTime(data.time_last_updated);
             const rate = data.rates[currency_two];
             //console.log(rate);
             rateElement.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
@@ -38,4 +65,5 @@ swap.addEventListener('click', () => {
     currencyElement_two.value = temp;
     calculate();
 })
+//updateDate();
 calculate();
